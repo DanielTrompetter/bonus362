13869d77bb59c486f7e89401b68e0f555febf304
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-03 11:27:17 +0200

    added bonus 3

--- test.dart
+++ test.dart
@@ -1,9 +1,28 @@
+// Bonus 2
 class Button
 {
   String title;
   int width, height, radius, fontsize, padding;
 
   Button(this.title, {required this.width, required this.height, required this.radius, required this.fontsize, required this.padding});
+}
+
+// Bonus 3
+class Rectangle
+{
+  double width, height;
+
+  Rectangle(this.width, this.height);
+
+  double Perimeter()
+  {
+    return 2*width+2*height;
+  }
+
+  double Area()
+  {
+    return width*height;
+  }
 }
 
 void main()
